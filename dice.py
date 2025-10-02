ec4a21108ac61cc6cf82f02683fd74f547384256
Ahmad Shalabi <ahmadshalabi050119@gmail.com>
2025-10-02 14:27:13 +0200

    lab3 dice ahmad

---
+++ dice.py
@@ -0,0 +1,31 @@
+import random
+
+def rollD4():
+    """
+    Roll a standard 6-sided dice.
+
+
+    Returns:
+        int: a random number between 1 and 6
+    """
+    return random.randint(1, 4)
+
+def rollD6():
+   
+    return random.randint(1, 6)
+
+def rollD8():
+   
+    return random.randint(1, 8)
+
+def rollD12():
+   
+    return random.randint(1, 12)
+
+def rollD20():
+   
+    return random.randint(1, 20)
+
+def rollD100():
+   
+    return random.randint(1, 100)
